3d08bfa122b59cfa6952d44a301b3eb9bec4a124
sittMinThar005 <sitt.thar0149@sbsuni.edu.vn>
2025-12-10 00:15:13 +0700

    Fix2

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle all routes by serving index.html (for client-side routing)
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
